053b92f17295f347525c5f94d5b0c28f33d8c096
Yasith Ishara Perera <yasithishara@gmail.com>
2024-09-03 13:14:18 +0530

    deployed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +6,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from "@angular/fire/firestore";
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ng-todo-list-59c31",
@@ -15,5 +16,8 @@ export const appConfig: ApplicationConfig = {
     "authDomain":"ng-todo-list-59c31.firebaseapp.com",
     "messagingSenderId":"1002148304886"})),
     provideAuth(() => getAuth()),
-  provideFirestore(()=>getFirestore())]
+  provideFirestore(()=>getFirestore()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 };
